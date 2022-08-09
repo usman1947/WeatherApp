a208d4faece2fbcb9b23f0a8893c67427a810179
Usman <usman22aslam@gmail.com>
2022-08-09 20:40:09 +0800

    refactoring to use functional component

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,17 @@
-import React, { Component} from 'react';
-import {ImArrowRight ,ImArrowLeft} from "react-icons/im"
-import {IconContext} from "react-icons"
-import {Card} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { ImArrowRight, ImArrowLeft } from "react-icons/im"
+import { IconContext } from "react-icons"
+import { Card } from 'react-bootstrap';
 import './App.css'
 import Data from './data.json';
 import { Chart } from "react-google-charts";
+import { useEffect } from 'react';
 
 const apiData = true;
-var average = [0,0,0,0,0]
+let average = [0,0,0,0,0]
 let tempArray = []
 
-var bar = [
+let bar = [
             ["Time","Temperature"],
             ["00:00",20],
             ["03:00",23],
@@ -22,177 +23,180 @@ var bar = [
             ["21:00",20],
 
             ]
-var tempType = "C"
-export default class Home extends Component {
+let tempType = "C"
 
-  constructor(props){
-    super(props);
-    this.state={
-        'cardNum' : 3, 
-        'firstCard':0,
-        'isC':true,
-        'isF':false,
-        'showBar':false,
-        'currentTemp':'C',
-        'barTemp' : 'C',
-        'activeCard':0
+export const Home = () => {
+
+    const [cardNum, setCardNum] = useState(3)
+    const [firstCard, setFirstCard] = useState(0)
+    const [isC, setIsC] = useState(true)
+    const [isF, setIsF] = useState(false)
+    const [currentTemp, setCurrentTemp] = useState('C')
+    const [activeCard, setActiveCard] = useState(0)
+
+    useEffect(() => {
+        Data?.forEach((data, i) => {
+            let adder = 0
+            for (let [key, value] of Object.entries(data)){
+                if (key !== "date")
+                    adder = adder + value 
+            average[i] = adder/8
+            }
+        })
+        handleBarShow(activeCard)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[Data])
+
+    function handleCardUp(){
+        let cn = cardNum
+        let fc = firstCard
+        let ac = activeCard
+
+        if (cn < 5){
+            setCardNum(cn + 1)
+            setFirstCard(fc+ 1)
         }
-
-    Data.forEach((data,i) => {
-        var adder = 0
-        for (let [key, value] of Object.entries(data)){
-            if (key !== "date")
-                adder = adder + value 
-        average[i] = adder/8
-        }
-    })
-    this.handleBarShow(this.state.activeCard)
-  }
-
-    handleCardUp = () => {
-        var cn = this.state.cardNum
-        var fc = this.state.firstCard
-        var ac = this.state.activeCard
-
-        if (cn < 5)
-            this.setState( {"cardNum" : cn + 1,"firstCard" : fc+ 1})
-
         if(ac < 4){
-            this.setState({activeCard:ac+1})
-            this.handleBarShow(ac+1)
+            setActiveCard(ac+1)
+            handleBarShow(ac+1)
         }
     }
 
-    handleCardDown = () => {
-        var cn = this.state.cardNum
-        var fc = this.state.firstCard
-        var ac = this.state.activeCard
+    function handleCardDown(){
+        let cn = cardNum
+        let fc = firstCard
+        let ac = activeCard
 
-        if (this.state.cardNum > 3)
-            this.setState( {"cardNum" : cn - 1,"firstCard" : fc- 1})
-
+        if (cardNum > 3){
+            setCardNum(cn - 1)
+            setFirstCard(fc - 1)
+        }
         if(ac > 0){
-            this.setState({activeCard:ac-1})
-            this.handleBarShow(ac-1)
+            setActiveCard(ac-1)
+            handleBarShow(ac-1)
         }
     }
 
-    handleTempType =(type) => {
-        if(type === "f" && this.state.currentTemp !== "F"){
+    function handleTempType(type){
+        if(type === "f" && currentTemp !== "F"){
             for (let i = 0; i < 5; i++){
                 average[i] = average[i] * 9/5 + 32
             }
             tempType = "F"
-            this.handleBarShow(this.state.activeCard)
-            this.setState({'isC':false,'isF':true,'currentTemp':"F"})
+            handleBarShow(activeCard)
+            setIsC(false)
+            setIsF(true)
+            setCurrentTemp("F")
         }
-        else if(type === "c" && this.state.currentTemp !== "C"){
-            for (let i = 0;i<5;i++){
+        else if(type === "c" && currentTemp !== "C"){
+            for (let i = 0; i < 5; i++){
                 average[i] = (average[i] - 32) * 5/9
             }
             tempType = "C"
-            this.handleBarShow(this.state.activeCard)
-            this.setState({'isC':true,'isF':false,'currentTemp':"C"})
+            handleBarShow(activeCard)
+            setIsC(true)
+            setIsF(false)
+            setCurrentTemp("C")
         }
     }
 
-    handleBarShow=(i)=>{
+    function handleBarShow(i){
         tempArray = []
         for (let [key, value] of Object.entries(Data[i])){
             if (key !== "date")
                 tempArray.push(value) 
         }
         if (tempType === "F"){
-            for (let i = 0;i<8;i++){
+            for (let i = 0; i<8; i++){
                 tempArray[i] = tempArray[i] * 9/5 + 32
             }
         }
-        for(let k=1; k<9;k++){
+        for(let k=1; k<9; k++){
             bar[k][1] = tempArray[k-1]+tempType
         }
-        this.setState({showBar:true,activeCard:i,barTemp:tempType})
+        setActiveCard(i)
     }
 
+    function onValueChange(){
+        console.log("onValueChange")
+    }
 
-    render() {
-     
-        if (!apiData)
-           return (<div className="App-header" >Loading ...</div>)
-        
-        return (
-
-            <div className="App" >
-                <form className="form">
-                    <div className="radio">
-                    <label>
-                        <input
-                        type="radio"
-                        value="Male"
-                        checked={this.state.isC}
-                        onChange={this.onValueChange}
-                        onClick={()=> this.handleTempType("c")}
-                        />
-                        Celsius
-                    </label>
-                    </div>
-                    <div className="radio">
-                    <label>
-                        <input
-                        type="radio"
-                        value="Female"
-                        checked={this.state.isF}
-                        onClick={()=> this.handleTempType("f")}
-                        onChange={this.onValueChange}
-                        />
-                        Fahrenheit
-                    </label>
-                    </div>
-                </form>
-                <div className="arrows">
-                    <IconContext.Provider value={{ style: {fontSize: '100px', color: "#003153"}}}>
-                        <div onClick={() => this.handleCardDown()}>
-                            <ImArrowLeft />
-                        </div>
-                    </IconContext.Provider>
-                    <IconContext.Provider value={{ style: {fontSize: '100px', color: "#003153"}}}>
-                        <div onClick={() => this.handleCardUp()}>
-                            <ImArrowRight />
-                        </div>
-                    </IconContext.Provider>
-                </div>
-                <div className="cardRow"> 
-                    {Data.map((day,i) => {
-                        return(i + this.state.firstCard < this.state.cardNum && (
-                                <Card className="card" key={i}>
-                                    <Card.Body className={i+this.state.firstCard === this.state.activeCard ? "cardActive":""} >
-                                        <Card.Text>
-                                            Temp:
-                                        </Card.Text>
-                                        <Card.Text>
-                                            {(average[i+this.state.firstCard]).toString()}
-                                        </Card.Text>
-                                        <Card.Text>
-                                            Date:
-                                        </Card.Text>
-                                        <Card.Text>
-                                            {Data[i+this.state.firstCard].date}
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card> )
-                        )
-                    })}
-                </div>
-        
-                <div className="chart">
-                    <Chart
-                        width={'1000px'}
-                        height={'500px'}
-                        chartType="Bar"
-                        loader={<div>Loading Chart</div>}
-                        data={bar}
+    if (!apiData)
+        return <div className="App-header" >Loading ...</div>
+    
+    return  <div className="App" >
+            <form className="form">
+                <div className="radio">
+                <label>
+                    <input
+                    type="radio"
+                    value="Male"
+                    checked={isC}
+                    onChange={onValueChange}
+                    onClick={()=> handleTempType("c")}
                     />
+                    Celsius
+                </label>
                 </div>
-            </div >
-        )
-    }
+                <div className="radio">
+                <label>
+                    <input
+                    type="radio"
+                    value="Female"
+                    checked={isF}
+                    onClick={()=> handleTempType("f")}
+                    onChange={onValueChange}
+                    />
+                    Fahrenheit
+                </label>
+                </div>
+            </form>
+            <div className="arrows">
+                <IconContext.Provider value={{ style: {fontSize: '100px', color: "#003153"}}}>
+                    <div onClick={() => handleCardDown()}>
+                        <ImArrowLeft />
+                    </div>
+                </IconContext.Provider>
+                <IconContext.Provider value={{ style: {fontSize: '100px', color: "#003153"}}}>
+                    <div onClick={() => handleCardUp()}>
+                        <ImArrowRight />
+                    </div>
+                </IconContext.Provider>
+            </div>
+            <div className="cardRow"> 
+                {Data.map((day,i) => {
+                    return(i + firstCard < cardNum && (
+                            <Card className="card" key={i}>
+                                <Card.Body className={i+firstCard === activeCard ? "cardActive":""} >
+                                    <Card.Text>
+                                        Temp:
+                                    </Card.Text>
+                                    <Card.Text>
+                                        {(average[i+firstCard]).toString()}
+                                    </Card.Text>
+                                    <Card.Text>
+                                        Date:
+                                    </Card.Text>
+                                    <Card.Text>
+                                        {Data[i+firstCard].date}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card> )
+                    )
+                })}
+            </div>
+    
+            <div className="chart">
+                <Chart
+                    width={'1000px'}
+                    height={'500px'}
+                    chartType="Bar"
+                    loader={<div>Loading Chart</div>}
+                    data={bar}
+                />
+            </div>
+        </div>
+    
 }
+
+export default Home;
